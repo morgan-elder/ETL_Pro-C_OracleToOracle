ac36fd3a632ef69e146dc6df6bc898b478254959
Douglas S. Elder <douglas.s.elder@boeing.com>
2020-12-19 18:25:09 -0600

    Added turning on debug if debug.txt has a Y in it Setup the defaults before invoking getopts - removed a lot of the env var's from the main and put them here so that they can be the defaults, but still be overridden by getopts switches.
    Added -o fileout switch and -s script switch so that the output file can
    easily be changed and the SQL*Plus script can easily be changed.
    
    Indicate that a log file has been created.  A sendmail could be added to
    send the log to the appropriate recipient.

--- scripts/sat_hotshot_request.sh
+++ scripts/sat_hotshot_request.sh
@@ -8,56 +8,79 @@
 #	12/18/2020	Douglas S. Elder	Tried to make it more usable
 #                               NOTE: file with oracle_account/password@tnsname
 #                               is missing - see PASSWORD_FILE below
+#                               The changes made enable this script to be
+#                               more generic and execute a SQL*Plus script
+#                               for any db
 #
 THIS=$(basename $0)
 # the original code was extracting the oracle_account/password@tnsname
 # from this file - i.e. the account needed to run the SQL*Plus Script
+BIN=/data/scm/sh
+export PATH=$BIN:$PATH
+if [[ -e $BIN/debug.txt ]] ; then
+  DEBUG=$(cat $BIN/debug.txt)
+  [[ "$DEBUG" == "Y" ]] && set -x
+fi
+# set defaults
 PASSWORD_FILE=/home/scmftp/.srfscm
-SCM_SQL="/data/scm/sql"
-SQLPLUS_SCRIPT=${SCM_SQL}/sat_hotshot_request.sql
+SQL_DIR="/data/scm/sql"
+SQLPLUS_SCRIPT=${SQL_DIR}/sat_hotshot_request.sql
+APPL=/APPL/rel/appl
+
+# source is equivalent to the dot . command
+# it just includes the code and does not issue a return code
+[[ "$DEBUG" == "Y" ]] && echo sourcing $APPL/pss_shared/public/set_pss_env_no_login
+source ${APPL}/pss_shared/public/set_pss_env_no_login
+
+[[ "$DEBUG" == "Y" ]] && echo sourcing /data/scm/public/setup_scm_sh
+source /data/scm/public/setup_scm_sh
+
+if [[ $PSS_ENV == "PRODN" ]] ; then
+  OUT_DIR=/data/scm/PROD
+else
+  OUT_DIR=/data/scm/TEST
+fi
+OUT_FILE=${OUT_DIR}/sat_hotshot_request.out
+echo "Processing in Environment "$PSS_ENV
+
+
 
 function usage {
   >&2 echo ""
-  >&2 echo "Usage $THIS [ -d -h -p oracct_password_file  ]"
+  >&2 echo "Usage $THIS [ -d -h -o fileout -p oracct_password_file -s script  ]"
   >&2 echo "where optional switch -d turns on debugging"
   >&2 echo "      optional switch -h displays this message"
+  >&2 echo "      optional switch -o fileout output from the"
+  >&2 echo "      SQL*Plus script will override $OUT_FILE"
   >&2 echo "      optional switch -p oracct_password_file"
   >&2 echo "      overrides the default file $PASSWORD_FILE"
+  >&2 echo "      optional switch -s script"
+  >&2 echo "      overrides the default file $SQLPLUS_SCRIPT"
+  >&2 echo "      script must specify the full path or path"
+  >&2 echo "      relative to the current working directory"
   >&2 echo ""
 }
 
-while getopts "dhp:" opt ; do
+while getopts "dho:p:s:" opt ; do
   case "$opt" in
     d) set -x; DEBUG=Y;;
+    o) OUT_FILE=$OPTARG;;
     p) PASSWORD_FILE=$OPTARG
        if [[ ! -e $PASSWORD_FILE ]] ; then
          >&2 echo "$PASSWORD_FILE does not exist"
          usage
        fi ;;
     h) usage; exit 0;;
+    s) SQLPLUS_SCRIPT=$OPTARG
+       if [[ ! -e $SQLPLUS_SCRIPT ]] ; then
+         >&2 echo "$SQLPLUS_SCRIPT does not exist"
+         usage
+       fi ;;
     *) usage; exit 4;;
   esac
 done
 shift $((OPTIND-1))
 
-function setup {
-
-  [[ "$DEBUG" == "Y" ]] && set -x
-
-  echo "###########################################################################"
-  echo "              Start SAT Hotshot Request - $(date)"
-  echo "###########################################################################"
-  typeset MCAIR=/mcair/rel/appl
-  # source is equivalent to the dot . command
-  # it just includes the code and does not issue a return code
-  [[ "$DEBUG" == "Y" ]] && echo sourcing $MCAIR/pss_shared/public/set_pss_env_no_login
-  source ${MCAIR}/pss_shared/public/set_pss_env_no_login
-
-  [[ "$DEBUG" == "Y" ]] && echo sourcing /data/scm/public/setup_scm_sh
-  source /data/scm/public/setup_scm_sh
-  echo "Processing in Environment "$PSS_ENV
-
-}
 
 # input /tmp file name to be created
 function prepareScript {
@@ -68,7 +91,7 @@ function prepareScript {
   # does the file with oracle_account/password@tnsname exist?
   if [[ -e "$PASSWORD_FILE" ]] ; then
     # write oracle_account/password@tnsname and the script to $TEMP
-    cat "$PASSWORD_FILE"  "$SQLPLUS_SCRIPT" > $TEMP
+    cat "$PASSWORD_FILE" "$SQLPLUS_SCRIPT"  > $TEMP
   else
     >&2 echo "Missing file $PASSWORD_FILE  with oracle_account/password@tnsname"
     RC=4
@@ -77,64 +100,63 @@ function prepareScript {
   return $RC
 }
 
+function showMsg {
+  typeset ACTION=$1
+  echo "###########################################################################"
+  echo "              $ACTION SAT Hotshot Request - $(date)"
+  echo "###########################################################################"
+}
+
 function main { 
   typeset RC=0 
-  typeset SCM_OUT=
 
-  setup
-
-  if [ $PSS_ENV = "PRODN" ]
-  then
-    export SCM_OUT=/data/scm/PROD
-  else
-    export SCM_OUT=/data/scm/TEST
-  fi
-
-  typeset HOTSHOT_OUT=${SCM_OUT}/sat_hotshot_request.out
+  showMsg "Start"
 
   echo ""
   echo "##################################################################"
-  echo "  Loading hotshot requests to New Breed interface"
   echo "  RUNNING: $SQLPLUS_SCRIPT"
   echo "           with credentials from $PASSWORD_FILE"
-  echo "  Output File: $HOTSHOT_OUT"
+  echo "  Output File: $OUT_FILE"
   echo "##################################################################"
   echo ""
 
-  typeset TEMP=$(mktemp)
+  typeset TEMP=$(mktemp).sql
 
   prepareScript "$TEMP"
   RC=$?
 
   if ((RC==0)) ; then
-    echo "Executing sqlplus -L @$TEMP $HOTSHOT_OUT"
-    sqlplus -L @$TEMP $HOTSHOT_OUT
+    [[ "$DEBUG" == "Y" ]] && echo "Executing sqlplus -L @$TEMP $OUT_FILE"
+    sqlplus -L @$TEMP $OUT_FILE
     RC=$?
     if ((RC==0)) ; then
-      if [[ -e "$HOTSHOT_OUT" ]] ; then
-        chmod 644 $HOTSHOT_OUT
+      if [[ -e "$OUT_FILE" ]] ; then
+        chmod 644 $OUT_FILE
         # all went well get rid of $TEMP
-	      echo "$SQLPLUS_SCRIPT exited successfully."
+	      echo "$SQLPLUS_SCRIPT exited successfully. (see $OUT_FILE)"
         rm $TEMP
      else
-	     >&2 echo "$SQLPLUS_SCRIPT failed to create $HOTSHOT_OUT"
-       >&2 echo "Check $TEMP script and $HOTSHOT_OUT"
+	     >&2 echo "$SQLPLUS_SCRIPT failed to create $OUT_FILE"
+       >&2 echo "Check $TEMP script and $OUT_FILE"
      fi
     else
-      echo "sqlplus -L @$TEMP $HOTSHOT_OUT failed with RC=$RC"
+      echo "sqlplus -L @$TEMP $OUT_FILE failed with RC=$RC"
     fi
   fi
+
+  echo ""
+
+  showMsg "End"
+
   return $RC
 }
 
-LOG=$(mktemp)
+LOG=$(mktemp).log
 
 set -o pipefail
 main 2>&1 | tee $LOG
 RC=$?
-if ((RC!=0)) ; then
-  echo ""
-  echo "Check log $LOG for errors"
-  echo ""
-fi
+echo ""
+echo "log $LOG created"
+echo ""
 exit $RC
